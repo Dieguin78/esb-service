822ec5320a5041600e940532efa9445ecf3714aa
dierez00 <dierez00@gmail.com>
2025-03-29 15:22:09 -0600

    rutas_internas

--- src/main/java/com/utd/ti/soa/esb_service/controller/ESBController.java
+++ src/main/java/com/utd/ti/soa/esb_service/controller/ESBController.java
@@ -66,10 +66,11 @@ public class ESBController {
         }
 
         String response = webClient.get()
-            .uri("https://users-production-2a02.up.railway.app/app/users/all") // Usa HTTPS
-            .exchangeToMono(clientResponse -> clientResponse.bodyToMono(String.class)) // Maneja redirección
+            .uri("http://users-production-2a02.up.railway.app/app/users/all") // Asegúrate de usar HTTP
+            .exchangeToMono(clientResponse -> clientResponse.bodyToMono(String.class)) // Manejo de respuesta
             .doOnError(error -> System.out.println("Error: " + error.getMessage()))
             .block();
+
 
         
         return ResponseEntity.ok(response);
